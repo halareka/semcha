b6e08ad1299110994ea25d608a8eddd66cb75dcc
halareka <149045655+halareka@users.noreply.github.com>
2023-11-20 16:17:46 +0700

    Update script.js

--- script.js
+++ script.js
@@ -16,15 +16,6 @@ document.getElementById('cat2').addEventListener('click', function(){
     
 const textmassiv = ["чо😀", "АААА😇😇😇😱", "ЧО😱", "бум😂🤣", "ьбуьуьу🤣", "5464😱😱😱", "ШТО😱", "пипяо😇", "спасяба🥰🥰", "вхуеит😱", "ам😇","ррр","бебебе🤓","ято🤓","фыр😇","фырфыр😱","ьббь🤨","уьуь😱","ухаха🧐","уха🤓","чоделать🤨","пдыщ😡","ясдох😡","ыы😡","ебануца😡","74 05 82🤨","DSGDSG🤨","КАК СПАТЬ😡","ФТОО😓","фтоо😓"]
 function textmain() {
-    var random = Math.floor(Math.random() * 30);
-    var randomtext = Math.floor(Math.random() * 30);
-    console.log(random);
-    console.log(randomtext);
-    document.getElementById('text').innerHTML = textmassiv[randomtext];
-    if (random >= 15) {
-        document.getElementById('text').style.transform = 'rotate(' + '-' + random + 'deg)';
-    } else {
-        document.getElementById('text').style.transform = 'rotate(' + random + 'deg)';
-    }
-    
+    var randomText = Math.floor(Math.random() * textmassiv.length);
+    document.querySelector('.text').innerHTML = textmassiv[randomText];
 }
